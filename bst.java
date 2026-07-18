d93d0f4bba2b6a7a68dd709e1bdb9cfcaa89b2db
Ayush Kumar <146323665+Ayush08k@users.noreply.github.com>
2026-07-18 11:52:59 +0530

    Add files via upload

---
+++ bst.java
@@ -0,0 +1,158 @@
+import java.util.ArrayList;
+
+public class bst {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node insert(Node root, int val) {
+        if (root == null) {
+            root = new Node(val);
+            return root;
+        }
+        if (root.data > val) {
+            root.left = insert(root.left, val);
+        } else {
+            root.right = insert(root.right, val);
+        }
+
+        return root;
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static boolean search(Node root, int key) {
+        if (root == null) {
+            return false;
+        }
+        if (root.data == key) {
+            return true;
+        }
+
+        if (key < root.data) {
+            return search(root.left, key);
+        } else {
+            return search(root.right, key);
+        }
+    }
+
+
+    public static Node delete(Node root, int val) {
+
+        if (root == null) {
+            return null;
+        }
+
+        if (root.data > val) {
+            root.left = delete(root.left, val);
+        } else if (root.data < val) {
+            root.right = delete(root.right, val);
+        } else {
+
+            //Case 1 If It is a leaf node
+            if (root.left == null && root.right == null) {
+                return null;
+            }
+
+            //Case 2 when the deleted node have 1 child only
+            if (root.left == null) {
+                return root.right;
+            } else if (root.right == null) {
+                return root.left;
+            }
+
+            //case 3 where it was a root node
+
+            Node IS = inorderSuccessor(root.right);
+            root.data = IS.data;
+            root.right = delete(root.right, IS.data);
+        }
+        return root;
+    }
+
+    public static Node inorderSuccessor(Node root) {
+        while (root.left != null) {
+            root = root.left;
+        }
+        return root;
+    }
+
+    public static void printInRange(Node root, int x, int y) {
+
+        if (root == null) {
+            return;
+        }
+        if (root.data >= x && root.data <= y) {
+            printInRange(root.left, x, y);
+            System.out.print(root.data + " ");
+            printInRange(root.right, x, y);
+        } else if (root.data >= y) {
+            printInRange(root.left, x, y);
+        } else {
+            printInRange(root.right, x, y);
+        }
+    }
+
+    public static void printPath(ArrayList<Integer> path){
+        for (int i=0; i<path.size(); i++){
+            System.out.print(path.get(i) + "->");
+        }
+        System.out.println();
+    }
+
+    public static Node print2Leaf(Node root, ArrayList<Integer> path){
+        if (root == null){
+            return null;
+        }
+        path.add(root.data);
+        if (root.left == null && root.right == null){
+            printPath(path);
+        }
+        else {
+            print2Leaf(root.left , path);
+            print2Leaf(root.right, path);
+        }
+        path.remove(path.size() -1);
+        return root;
+    }
+
+
+    public static void main(String[] args) {
+        int values[] = {8, 5, 3, 1, 4, 6, 10, 11, 14};
+        Node root = null;
+        int key = 9;
+        for (int i = 0; i < values.length; i++) {
+            root = insert(root, values[i]);
+        }
+
+        if (search(root, key)){
+            System.out.println("found");
+        } else {
+            System.out.println("Not Found");
+        }
+        inorder(root);
+        System.out.println();
+
+        inorder(root);
+        System.out.println();
+        delete(root, 5 );
+        printInRange(root, 4, 5);
+
+        print2Leaf(root, new ArrayList<>());
+
+    }
+}
